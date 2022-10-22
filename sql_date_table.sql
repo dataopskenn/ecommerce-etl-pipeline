2c645edc99ea9db897f55796117c0792ce85a9fe
dataopskenn <108646604+dataopskenn@users.noreply.github.com>
2022-10-22 20:05:09 +0100

    update

--- sql_date_table.sql
+++ sql_date_table.sql
@@ -9,9 +9,9 @@ CREATE TABLE IF NOT EXISTS dataopskenn_staging.dim_dates
         public_holiday           BOOLEAN NOT NULL
     );
     
-    ALTER TABLE user_9999_staging.dim_dates
-    OWNER to postgres;
-    INSERT INTO user_9999_staging.dim_dates
+ALTER TABLE user_9999_staging.dim_dates
+OWNER to postgres;
+INSERT INTO user_9999_staging.dim_dates
     SELECT 
        datum AS calendar_dt,
        EXTRACT(YEAR FROM datum) AS year_num,
